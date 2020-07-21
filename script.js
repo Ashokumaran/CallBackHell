f2dd8390b6fa2ca69a90e7f4b7d091fff8cfb9f4
Ashok Kumaran <iamashokachu@outlook.com>
2020-07-22 01:40:36 +0530

    CallBack Hell - Without named functions / only setTimeout();

--- script.js
+++ script.js
@@ -1,3 +1,70 @@
+setTimeout(() => {
+    var count=5;
+        document.getElementById("number").innerText = count;
+        count--;
+        setTimeout(() => {
+            document.getElementById("period1").innerText = ".";
+            setTimeout(() => {
+                document.getElementById("period2").innerText = ".";
+                setTimeout(() => {
+                    document.getElementById("number").innerText = count;
+                    count--;
+                    document.getElementById("period1").innerHTML = "&nbsp;";
+                    document.getElementById("period2").innerHTML = "&nbsp;";
+                    setTimeout(() => {
+                        document.getElementById("period1").innerText = ".";
+                        setTimeout(() => {
+                            document.getElementById("period2").innerText = ".";
+                            setTimeout(() => {
+                                document.getElementById("number").innerText = count;
+                                count--;
+                                document.getElementById("period1").innerHTML = "&nbsp;";
+                                document.getElementById("period2").innerHTML = "&nbsp;";
+                                setTimeout(() => {
+                                    document.getElementById("period1").innerText = ".";
+                                    setTimeout(() => {
+                                        document.getElementById("period2").innerText = ".";
+                                        setTimeout(() => {
+                                            document.getElementById("number").innerText = count;
+                                            count--;
+                                            document.getElementById("period1").innerHTML = "&nbsp;";
+                                            document.getElementById("period2").innerHTML = "&nbsp;";
+                                            setTimeout(() => {
+                                                document.getElementById("period1").innerText = ".";
+                                                setTimeout(() => {
+                                                    document.getElementById("period2").innerText = ".";
+                                                    setTimeout(() => {
+                                                        document.getElementById("number").innerText = count;
+                                                        count--;
+                                                        document.getElementById("period1").innerHTML = "&nbsp;";
+                                                        document.getElementById("period2").innerHTML = "&nbsp;";
+                                                        setTimeout(() => {
+                                                            document.getElementById("period1").innerText = ".";
+                                                            setTimeout(() => {
+                                                                document.getElementById("period2").innerText = ".";
+                                                                setTimeout(() => {
+                                                                    document.getElementById("number").innerHTML = "Time's Up!!!";
+                                                                    document.getElementById("period1").innerHTML = "";
+                                                                    document.getElementById("period2").innerHTML = "";
+                                                                }, 500);
+                                                            }, 250);
+                                                        }, 250);
+                                                    }, 500);
+                                                }, 250);
+                                            },250);
+                                        },500);
+                                    },250);
+                                },250);
+                            },500);
+                        },250);
+                    },250)
+                },500);
+            },250);
+        },250);  
+    },500);
+
+
+/*
 function counter(val,callback){
     return callback(val-1);
 }
@@ -17,3 +84,4 @@ for(var i=10;i>=0;i--){
             }
     });
 };
+*/
